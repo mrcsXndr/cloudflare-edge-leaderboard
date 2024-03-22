c3fecc3d0bd1c3fa097928e53d7651b7cc57a577
mArvidsson <arvidsson.marcus@hotmail.com>
2024-03-22 23:07:22 +0100

    Initial commit

--- main.js
+++
@@ -1,8 +0,0 @@
-import Leaderboard from './Leaderboard';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const leaderboardElement = document.getElementById('leaderboard');
-    // Updated to pass an options object with "fields"
-    const options = { fields: ["Name", "Points", "Timestamp"] };
-    const leaderboard = new Leaderboard(leaderboardElement, options, 3, 10, true);
-});
